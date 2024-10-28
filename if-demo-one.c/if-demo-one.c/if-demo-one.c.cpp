63bd7b6f0ddf79e51aa886baa111c5b25d20f353
pangfeng712 <1839424376@qq.com>
2024-10-28 23:45:07 +0800

    Add files via upload

---
+++ if-demo-one.c/if-demo-one.c/if-demo-one.c.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int rowA = 5;
+	int lineA = 6;
+	int rowB = 6;
+	int lineB = 7;
+	if ((rowA == rowB) && (lineA - lineB == 1 || lineA - lineB == -1))
+	{
+		printf("我会开心的看电影");
+	}
+
+	else
+	{
+		printf("我会开心的打游戏");
+	}
+	return 0;
+}
