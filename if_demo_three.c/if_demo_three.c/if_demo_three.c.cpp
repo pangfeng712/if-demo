9c61af1e10f439f890d74f02eaae870a72256227
pangfeng712 <1839424376@qq.com>
2024-10-29 17:47:36 +0800

    Add files via upload

---
+++ if_demo_three.c/if_demo_three.c/if_demo_three.c.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int score;
+	printf("请输入期末成绩：");
+	scanf_s("%d", &score);
+	if (score >= 85 && score <= 100)
+	{
+		printf("A");
+	}
+	else if (score >= 70 && score <= 84)
+	{
+		printf("B");
+	}
+	else if (score >= 60 && score <= 69)
+	{
+		printf("C");
+	}
+	else
+	{
+		printf("D");
+	}
+	return 0;
+}
---
+++ if_demo_two.c/if_demo_two.c/if_demo_two.c.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int money;
+	printf("请输入你氪金的金额：");
+	scanf_s("%d", &money);
+	if (money == 0)
+	{
+		printf("0充玩家");
+	}
+	else if (money >= 1 && money <= 100)
+	{
+		printf("尊贵的VIP1");
+	}
+	else if (money >= 100 && money <= 499)
+	{
+		printf("尊贵的VIP2");
+	}
+	else if (money >= 500 && money <= 1000)
+	{
+		printf("尊贵的VIP3");
+	}
+	else if (money >= 1000 && money <= 1999)
+	{
+		printf("尊贵的VIP4");
+	}
+	else
+	{
+		printf("尊贵的VIP5");
+	}
+	return 0;
+}
